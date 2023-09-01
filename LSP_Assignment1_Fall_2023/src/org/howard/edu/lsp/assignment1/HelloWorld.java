ecafaf974b392ce5a789e23aee41991a033eaefa
efdubya4 <90352730+efdubya4@users.noreply.github.com>
2023-08-31 20:11:01 -0400

    initial commit

---
+++ LSP_Assignment1_Fall_2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+/**
+ * Foster Ware IV
+ * @param args
+ */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
