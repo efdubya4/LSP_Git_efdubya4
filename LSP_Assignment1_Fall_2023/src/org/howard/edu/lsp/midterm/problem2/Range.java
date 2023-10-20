e8f3103007fd9408f5083db10faba21033732545
efdubya4 <90352730+efdubya4@users.noreply.github.com>
2023-10-19 21:38:34 -0400

    midterm exam commit

--- LSP_Assignment1_Fall_2023/src/org/howard/edu/lsp/midterm/problem2/Range.java
+++ LSP_Assignment1_Fall_2023/src/org/howard/edu/lsp/midterm/problem2/Range.java
@@ -1,5 +1,23 @@
 package org.howard.edu.lsp.midterm.problem2;
 
 public class Range {
+    private int start;
+    private int end;
 
+    public Range(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public boolean isValueInRange(int value) {
+        return value >= start && value <= end;
+    }
+
+    public boolean doRangesOverlap(Range otherRange) {
+        return !(otherRange.end < start || otherRange.start > end);
+    }
+
+    public int size() {
+        return end - start + 1;
+    }
 }
